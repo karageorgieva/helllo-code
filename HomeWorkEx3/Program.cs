d7e1550665145f9b99e43200f6edc0a2b3ab4f11
Darya <karageorgieva@icloud.com>
2022-12-03 19:14:25 +0300

    четные числа

---
+++ HomeWorkEx3/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 5;
+int b = 8;
+int countB = 1;
+int countA = 1;
+
+while(countA <= a)
+{
+    if((countA % 2) == 0)
+    {
+    Console.Write(countA);    
+    Console.Write(" ");
+    }
+    countA++;
+}
+Console.WriteLine(" ");
+while(countB <= b)
+{
+    if((countB % 2) == 0)
+    {
+    Console.Write(countB);    
+    Console.Write(" ");
+    }
+    countB++;
+}
