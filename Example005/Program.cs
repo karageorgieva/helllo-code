edbf2f6ff1c1d5bcb38445013176cb79b47d1690
Darya <karageorgieva@icloud.com>
2022-12-03 16:13:57 +0300

    ex 005

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Write down username");
+string username= Console.ReadLine();
+
+if(username.ToLower() == "gosha")
+{
+    Console.WriteLine("WOW this is Gosha");
+}
+else 
+{
+    Console.WriteLine("Hi, ");
+    Console.WriteLine(username);
+}
