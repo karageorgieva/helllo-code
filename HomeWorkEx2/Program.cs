da2e796d15a1b55d42294cacea2f18402bf33dda
Darya <karageorgieva@icloud.com>
2022-12-03 18:38:00 +0300

    YES or NO

---
+++ HomeWorkEx2/Program.cs
@@ -0,0 +1,30 @@
+﻿int a = 4;
+int b = -3;
+int c = 7;
+
+if((a % 2) == 0)
+{
+    Console.WriteLine("YES");
+}
+else 
+{
+    Console.WriteLine("NO");
+}
+
+if((b % 2) == 0)
+{
+    Console.WriteLine("YES");
+}
+else 
+{
+    Console.WriteLine("NO");
+}
+
+if((c % 2) == 0)
+{
+    Console.WriteLine("YES");
+}
+else 
+{
+    Console.WriteLine("NO");
+}
