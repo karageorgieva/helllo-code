7a64a5ba3345fb0dde4022d41b7c7ec69be00a5d
Darya <karageorgieva@icloud.com>
2022-12-03 18:09:24 +0300

    3 max numbers

---
+++ HomeWorkEx1/Program.cs
@@ -0,0 +1,35 @@
+﻿int a = 2;
+int b = 3;
+int c = 7;
+
+int max1 = a;
+if (a > max1) max1 = a;
+if (b > max1) max1 = b;
+if (c > max1) max1 = c;
+
+Console.Write("max1 = ");
+Console.WriteLine(max1);
+
+int d = 44;
+int e = 5;
+int f = 78;
+
+int max2 = a;
+if (d > max2) max2 = d;
+if (e > max2) max2 = e;
+if (f > max2) max2 = f;
+
+Console.Write("max2 = ");
+Console.WriteLine(max2);
+
+int g = 22;
+int j = 3;
+int k = 9;
+
+int max3 = a;
+if (g > max3) max3 = g;
+if (j > max3) max3 = j;
+if (k > max3) max3 = k;
+
+Console.Write("max3 = ");
+Console.WriteLine(max3);
