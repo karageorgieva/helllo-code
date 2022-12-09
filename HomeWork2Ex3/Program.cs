750459582a1e93d0981fb43da4b2d787e3f6b2f8
Darya <karageorgieva@icloud.com>
2022-12-10 00:42:13 +0300

    home work 2 ex 3

---
+++ HomeWork2Ex3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Ввидите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number == 6 ||  Number == 7){
+
+  Console.WriteLine("да, выходной");
+
+}
+
+else if (Number < 1 || Number >7) {
+
+  Console.WriteLine("Sorry, такого дня не существует");
+
+}
+else  {
+
+  Console.WriteLine("не выходной");
+
+}
