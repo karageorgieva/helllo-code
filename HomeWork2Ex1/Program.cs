2cf6abb3aeeaa57abe1fc400098fd36b9fa05243
Darya <karageorgieva@icloud.com>
2022-12-09 23:10:34 +0300

    Home work 2 ex 1

---
+++ HomeWork2Ex1/Program.cs
@@ -0,0 +1,18 @@
+﻿int SecondNumb (int arg1)
+{
+    int result = arg1 / 10;
+    result = result % 10;
+    return result;
+}
+
+int a = 456;
+int b = 782;
+int c = 918;
+
+int a1 = SecondNumb (a);
+int b1 = SecondNumb (b);
+int c1 = SecondNumb (c);
+
+Console.WriteLine (a1);
+Console.WriteLine (b1);
+Console.WriteLine (c1);
