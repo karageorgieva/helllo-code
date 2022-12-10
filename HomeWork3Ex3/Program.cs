17bfa47af2f3d876d9db09d45e1e3a9376be87e8
Darya <karageorgieva@icloud.com>
2022-12-10 20:44:27 +0300

    home work 3 ex 3

---
+++ HomeWork3Ex3/Program.cs
@@ -0,0 +1,13 @@
+﻿void kub(int number)
+{
+for (int i = 1; i <= number; i++)
+{ 
+    Console.Write($"{i*i*i} ");
+
+}
+    Console.WriteLine("");
+}
+Console.Write("3 --> ");
+kub(3);
+Console.Write("5 --> ");
+kub(5);
