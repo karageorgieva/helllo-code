8b50985cfc3074300e57fbd1b8255f548b5ea04f
Darya <karageorgieva@icloud.com>
2022-12-17 12:04:13 +0300

    ex3

---
+++ EX3Homework4/Program.cs
@@ -0,0 +1,11 @@
+﻿ int TODEC(int[] value)
+{
+    int result = 0;
+    int count = value.Length;
+    for (int i = 0; i < count; i++)
+{
+    result += value[i] * double(15, count - 1 - i);
+}
+    return result;
+}
+Console.WriteLine(TODEC(new int[] {9, 4, 12}));
