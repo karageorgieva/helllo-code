d096323cf37b17ec46cfb2c3c8334c8bb253653c
Darya <karageorgieva@icloud.com>
2022-12-17 10:55:46 +0300

    Ex 2

---
+++ HomeWork4Ex2/Program.cs
@@ -0,0 +1,34 @@
+﻿int number = ReadInt("Введите число: ");
+int count = NumberKol (number);
+SumNumbers(number, count);
+
+
+int ReadInt(string chislo)
+{
+    Console.Write(chislo);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int NumberKol(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+
+void SumNumbers(int n, int count)
+{
+    int sum = 0;
+    for (int i = 1; i <= count; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
